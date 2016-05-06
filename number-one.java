c83c0ec15f8e6dea451624727345b21a263459d4
Hongbo Jing <hongboing@Hongbo-Jings-MacBook-Pro.local>
2016-05-05 22:44:25 -0400

    add some java syntax knowledge

--- number-one.java
+++ number-one.java
@@ -36,3 +36,15 @@ class Solution {
         }
     }
 }
+
+// && (logical and)
+// || (logical or)
+// ! (logical not)
+
+// public int indexOf(int ch): Returns the index within this string of the first occurrence of the specified character or -1 if the character does not occur.
+//
+// public int indexOf(int ch, int fromIndex): Returns the index within this string of the first occurrence of the specified character, starting the search at the specified index or -1 if the character does not occur.
+//
+// int indexOf(String str): Returns the index within this string of the first occurrence of the specified substring. If it does not occur as a substring, -1 is returned.
+//
+// int indexOf(String str, int fromIndex): Returns the index within this string of the first occurrence of the specified substring, starting at the specified index. If it does not occur, -1 is returned.
